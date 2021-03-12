de81bfbd088a3be1fbc2dadaac1aa0a07bb7f96b
Doug Blewett <36805170+blewett@users.noreply.github.com>
2021-03-12 00:54:48 -0800

    Add files via upload

---
+++ section-shell.py
@@ -0,0 +1,250 @@
+"""
+  section-shell.py: Original work Copyright (C) 2021 by Doug Blewett
+
+MIT License
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+  run shell files in sections
+
+"""
+import sys
+import os
+
+section_tag = "# section "
+section_tag_len = 10
+
+def read_content(file):
+    try:
+        with open(file) as f:
+            content = f.readlines()
+    except IOError:
+        print(prog + ": could open the file: \"" + file + "\"")
+        exit()
+
+    f.close()
+    content = [line.strip() for line in content]
+    return content
+
+def find_longest_section_name(content):
+    max = 0
+    for line in content:
+        where = line.find(section_tag)
+        if where == 0:
+            line = line[section_tag_len:].strip()
+            where = line.find(":")
+            section = line[:where].strip()
+            length = len(section)
+            if length > max:
+                max = length
+    return max
+
+def main(filename, skip_section, skip_sub):
+    content = read_content(sys.argv[1])
+    if len(content) == 0:
+        print(prog + ": the file " + sys.argv[1] + " has no content lines")
+        exit(1)
+
+    base_indent_len = find_longest_section_name(content)
+    if base_indent_len == 0:
+        print(prog + ": the file " + sys.argv[1] + " has no section entries")
+        exit(1)
+
+    base_indent = " " * base_indent_len
+
+    # skip by default until the skip_section has been found
+    skip = True
+    execute = False
+    section = ""
+    clear_next_section = False
+    sub = 0
+    for line in content:
+        # skip blank lines
+        if len(line) == 0:
+            continue
+
+        # look for section headers at the front of the line
+        where = line.find(section_tag)
+        if where == 0:
+            sub = 0
+            line = line[section_tag_len:].strip()
+            where = line.find(":")
+            section = line[:where].strip()
+            indent = " " * (base_indent_len - len(section))
+
+            cd_string = ""
+            where = line.find("DO CD ")
+            if where > 0:
+                cd_string = str(line[where + 6:])
+                cd_string = cd_string.strip()
+
+            # are we skipping sections
+            if clear_next_section == True:
+                skip_section = ""
+                clear_next_section = False
+
+            if skip_section != "":
+                if skip_section != section:
+                    print("* " + indent + line)
+                    print("skipped")
+                    continue
+
+                clear_next_section = True
+
+                if skip_sub > sub:
+                    print("")
+                    print("skipping until " +
+                          str(section) + "." + str(skip_sub))
+                else:
+                    skip_section = ""
+
+            execute = False
+            skip = False
+            while True:
+                print("")
+                print("* " + indent + line)
+                text = input("    e(xecute), s(kip), q(uit)?")
+
+                if text[0] == 'q':
+                    exit(0)
+                if text[0] == 'e' or text[0] == 'x':
+                    execute = True
+                    if cd_string != "":
+                        try:
+                            os.chdir(cd_string)
+                        except:
+                            print("")
+                            print("change directory to " + cd_string + " failed")
+                            print("")
+
+                    cd_string = ""
+                    break
+                if text[0] == 's':
+                    skip = True
+                    break
+
+                print("")
+
+            continue
+
+        if section == "":
+            continue
+
+        if line[0] != '#':
+            sub += 1
+
+        if skip == True:
+            continue
+
+        if skip_section != "" and skip_section != section:
+            continue
+
+        indent = " " * (base_indent_len - len(section)) + section
+        if line[0] == '#':
+            print("    " + base_indent + line)
+            continue
+        else:
+            print(indent + "." + str(sub) + ": " + line)
+
+        if skip_section != "":
+            if sub >= skip_sub:
+                skip_section = ""
+            else:
+                print("skipped")
+                print("")
+                continue
+
+        if execute == True:
+            print("")
+            os.system(line)
+            print("")
+            continue
+
+    print("done")
+# end of main
+
+def help(prog):
+    print("")
+    print("  " + prog + ":")
+    print("")
+    print("   The benefit of \"section-shell.py\" is that it helps organize large")
+    print("   and complex tasks that require user interaction.  The user provides")
+    print("   a shell file containing all of the steps required for the task even")
+    print("   though success in completing the task in a single run is unlikely.")
+    print("   Sections can be easily repeated until the processing is complete.")
+    print("")
+    print("   This program runs sections of a named shell file.  The syntax of the ")
+    print("   shell file is that of named sections.  The following line might be ")
+    print("   used to start a section to perform installs")
+    print("")
+    print("     # section 1: install python3 and build utilities")
+    print("     date")
+    print("     touch timestamp")
+    print("     make install")
+    print("")
+    print("   section-shell.py will display the following for that section:")
+    print("")
+    print("     *  1: install python3 and build utilities")
+    print("     e(xecute), s(kip), q(uit)?")
+    print("")
+    print("   The user may elect to execute or skip the shell commands in a section.")
+    print("   There may be multiple commands in each section.  The commands are")
+    print("   numbered as they are executed.")
+    print("")
+    print("   The arguments for section-shell.py are \"filename [section] [sub command #]\".")
+    print("   The section and the sub number are optional and if provided will result in")
+    print("   processing starting in the named section and at the sub number (shell")
+    print("   command).")
+
+#
+# parse program argument
+#
+argc = len(sys.argv)
+prog = sys.argv[0]
+
+if (argc == 1 or argc > 4):
+    print(prog + ": " + prog + " filename [section] [sub command #]")
+    print(prog + ":   the first argument should be a filename.")
+    exit(1)
+
+filename = ""
+section = ""
+sub = 0
+
+for arg in sys.argv:
+    if arg == "?" or arg == "-?" or arg == "-h" or arg == "help":
+        help(prog)
+        exit(1)
+
+if (argc >= 2):
+    filename = sys.argv[1]
+
+if (argc >= 3):
+    section = sys.argv[2]
+
+if (argc == 4):
+    sub = sys.argv[3]
+    if sub.isdigit() == False:
+        print(prog + ": " + prog + " filename [section] [sub command #]")
+        print(prog + ":   the sub command # must be a number.")
+        exit(1)
+    sub = int(sub)
+
+main(filename, section, sub)
+
---
+++ tensor-install-ubuntu.sh
@@ -0,0 +1,139 @@
+#
+# run this file as a shell to get python3 installed:
+#
+#     sh tensor-install-ubuntu.sh
+#
+# run this shell script section by section with the simple python
+# controller:
+#
+#    python3 section-shell.py tensor-install-ubuntu.sh 2
+#
+# This file contains the setup instructions from the TensorFlow
+# webpage listed below:
+# 
+#    https://www.tensorflow.org/install/source
+# 
+# These instructions are for compiling the TensorFlow system for linux
+# based computers:
+# 
+# section 1: install python3 and build utilities
+sudo apt install python3-dev python3-pip
+
+pip3 install -U --user pip numpy wheel
+pip3 install -U --user keras_preprocessing --no-deps
+pip3 install testresources
+pip3 install matplotlib
+
+sudo apt install build-essential
+sudo apt install git
+
+exit(0)
+
+#
+# installing bazel
+#
+
+#
+# https://docs.bazel.build/versions/master/install-ubuntu.html
+# do the first two steps - the compile will request an earlier
+#  simple apt install operations
+#
+
+# section 2a: installing bazel support
+sudo apt install curl gnupg
+curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor > bazel.gpg
+sudo mv bazel.gpg /etc/apt/trusted.gpg.d/
+echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+
+# section 2b: install and upgrade bazel
+sudo apt update && sudo apt install bazel
+
+#
+# Once installed, you can upgrade to a newer version of Bazel as part
+# of your normal system updates:
+#
+# section 2c: installing bazel full-upgrade
+sudo apt update && sudo apt full-upgrade
+sudo apt update && sudo apt install bazel-3.7.2
+
+#
+# The bazel package will always install the latest stable version of
+# Bazel. You can install specific, older versions of Bazel in addition
+# to the latest one like this:
+#
+# sudo apt install bazel-1.0.0
+#
+# This will install Bazel 1.0.0 as /usr/bin/bazel-1.0.0 on your
+# system. This can be useful if you need a specific version of Bazel
+# to build a project, e.g. because it uses a .bazelversion file to
+# explicitly state with which Bazel version it should be built.
+#
+# Optionally, you can set bazel to a specific version by creating a
+# symlink:
+#
+# sudo ln -s /usr/bin/bazel-1.0.0 /usr/bin/bazel
+# bazel --version  # 1.0.0
+#
+
+#
+# back to the source install - download the tensorflow source
+#
+# section 3: download tensorflow
+mkdir tensorflow
+git clone https://github.com/tensorflow/tensorflow.git
+
+#
+# the DO CD - MAGIC SECTION TITLE changes the current directory!
+#    - REQUIRED for the rest of the process
+
+# section 4a: the cd in the next step is required
+pwd
+
+#
+# we are moving the the tensorflow directory to configure and
+# compile the system
+#
+
+# section 4b: DO CD tensorflow
+pwd
+
+# configure the build
+#
+# determine the architecture with which to configure
+#
+# gcc -march=native -Q --help=target | grep -v valid | grep march
+#
+# section 5: determine the architecture
+echo march=`gcc -march=native -Q --help=target | grep -v valid | grep march | sed -e "s/ //g" -e "s/\t//g" -e "s/-march=//"`
+echo "The current recommended ation is to use -march=native, unless there is another specific need."
+
+# section 6a: prepare for ./configure
+march=`gcc -march=native -Q --help=target | grep -v valid | grep march | sed -e 's/ //g' -e 's/\t//g' -e 's/-march=//'`;echo "  The next section runs the ./configure script."; echo "  When ./configure asks for options enter: -march=native -Wno-sign-compare"; echo "  Say no to ROCm, CUDA, and all unless you plan to run with a CUDA board "; echo "  (you do not)."
+
+# section 6b: run ./configure
+./configure
+
+# section 7: build non pip standalone tensorflow
+bazel build --verbose_failures --config=opt --config=noaws --config=nogcp --config=nohdfs --config=nonccl //tensorflow:tensorflow
+
+# section 8: Build the pip package
+bazel build --verbose_failures --config=opt --config=noaws --config=nogcp --config=nohdfs --config=nonccl //tensorflow/tools/pip_package:build_pip_package
+
+# section 9: bazel-bin Build Package
+./bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
+
+# section 10: install the newly created wheel file
+pip3 install tensorflow-2.5.0-cp38-cp38-linux_x86_64.whl 
+
+# section 11: stop here
+#
+#
+# https://www.tensorflow.org/install
+# https://www.tensorflow.org/learn
+# https://www.tensorflow.org/overview
+# 
+# https://www.tensorflow.org/lite
+# https://www.tensorflow.org/lite/guide
+# 
+# https://www.tensorflow.org/resources/learn-ml
+
